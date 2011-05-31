ea7f82b828046a54ceda42cd556242f648323441
Bob Kuo <bobjkuo@gmail.com>
2011-05-30 20:27:04 -0500

    wrap that bad boy in a function

--- gmph2ncidef.pl
+++ gmph2ncidef.pl
@@ -12,7 +12,7 @@ if (scalar @ARGV != 1 || ! -e $ARGV[0]) {
 }
 
 my $filename = $ARGV[0];
-my %functions;
+
 my %mappings = (
   void => 'v',
   # 'i' types
@@ -66,54 +66,62 @@ my @blacklist = qw(
   mpz_set_f
 );
 
-open my $gmp_header, '<', $filename;
+my %functions = process_gmph($filename);
 
-while(<$gmp_header>) {
-  chomp;
-  next if $_ !~ m/^#/;
-  # note: in the future, we will look for more functions
-  # and not just mpz types
-  my $prefix = '_?mpz';
-  # does the line match a C-style declare?
-  if ($_ =~ m/#define ($prefix\S+) (__\S+)/) {
-    # $1 is the convenient name used everywhere else
-    my $convenient_name = $1;
-    # $2 is the name we must use in the NCI def
-    my $internal_name = $2;
-    # skip if it's on our blacklist
-    next if any { $convenient_name eq $_ } @blacklist;
-    $functions{$convenient_name}{'internal_name'} = $internal_name;
-    # read until the next blank line into a definition
-    my $definition;
-    while ((my $following_line = <$gmp_header>) !~ m/^$/) {
-      chomp $following_line;
-      # some functions have complex conditions to check if
-      # __GMP_DECLSPEC should be called ...
-      # let's get these functions signatures regardless of those conditions
-      next if $following_line =~ m/^(#if|#endif)/;
+sub process_gmph {
+  my $filename = shift;
 
-      # remove inline comments from the definition
-      $following_line =~ s!/\*.*\*/!!;
-      $definition .= $following_line;
-    }
-    # process the definition into a return type and a method signature
-    if ($definition =~ m/__GMP_DECLSPEC\s+(.+)$convenient_name __GMP_PROTO \(\((.+)\)\)/) {
-      my $return_type      = $1;
-      my $method_signature = $2;
+  open my $gmp_header, '<', $filename;
 
-      # process return_type and method_signature - we need to convert
-      # it from a valid C type to an NCI def - ie. void -> v
-      my $converted_return_type = process_types($return_type);
-      $functions{$convenient_name}{'return_type'}      = $converted_return_type;
-      my $converted_method_signature = join '', map { process_types($_) } split /,/, $method_signature;
-      $functions{$convenient_name}{'method_signature'} = $converted_method_signature;
-    } else {
-      warn "Line does not match function definition: $definition";
+  while(<$gmp_header>) {
+    chomp;
+    next if $_ !~ m/^#/;
+    # note: in the future, we will look for more functions
+    # and not just mpz types
+    my $prefix = '_?mpz';
+    # does the line match a C-style declare?
+    if ($_ =~ m/#define ($prefix\S+) (__\S+)/) {
+      # $1 is the convenient name used everywhere else
+      my $convenient_name = $1;
+      # $2 is the name we must use in the NCI def
+      my $internal_name = $2;
+      # skip if it's on our blacklist
+      next if any { $convenient_name eq $_ } @blacklist;
+      $functions{$convenient_name}{'internal_name'} = $internal_name;
+      # read until the next blank line into a definition
+      my $definition;
+      while ((my $following_line = <$gmp_header>) !~ m/^$/) {
+        chomp $following_line;
+        # some functions have complex conditions to check if
+        # __GMP_DECLSPEC should be called ...
+        # let's get these functions signatures regardless of those conditions
+        next if $following_line =~ m/^(#if|#endif)/;
+
+        # remove inline comments from the definition
+        $following_line =~ s!/\*.*\*/!!;
+        $definition .= $following_line;
+      }
+      # process the definition into a return type and a method signature
+      if ($definition =~ m/__GMP_DECLSPEC\s+(.+)$convenient_name __GMP_PROTO \(\((.+)\)\)/) {
+        my $return_type      = $1;
+        my $method_signature = $2;
+
+        # process return_type and method_signature - we need to convert
+        # it from a valid C type to an NCI def - ie. void -> v
+        my $converted_return_type = process_types($return_type);
+        $functions{$convenient_name}{'return_type'}      = $converted_return_type;
+        my $converted_method_signature = join '', map { process_types($_) } split /,/, $method_signature;
+        $functions{$convenient_name}{'method_signature'} = $converted_method_signature;
+      } else {
+        warn "Line does not match function definition: $definition";
+      }
     }
   }
-}
 
-close $gmp_header;
+  close $gmp_header;
+
+  return %functions;
+}
 
 sub process_types {
   my $type = shift;
