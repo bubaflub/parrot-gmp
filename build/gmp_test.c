f0a0a00991995017032f5cd6cd8e99737f29f6bb
Bob Kuo <bobjkuo@gmail.com>
2011-06-30 22:25:31 -0500

    C script to get some system dependent values from GMP

---
+++ build/gmp_test.c
@@ -0,0 +1,9 @@
+#include <gmp.h>
+#include <stdio.h>
+
+int main() {
+  printf("%i %i %i\n", __GNU_MP_VERSION, __GNU_MP_VERSION_MINOR, __GNU_MP_VERSION_PATCHLEVEL);
+  printf("%zu %zu\n", sizeof(mp_size_t), sizeof(mp_exp_t));
+  printf("%zu\n", sizeof(size_t));
+  printf("%zu\n", sizeof(mp_limb_t));
+}
